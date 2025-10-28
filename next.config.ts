0e2adc4492c9772d7608808056b859d5d00eec7a
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2025-10-27 21:48:36 -0400

    fixed prisma error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.BLOB_HOSTNAME || 'x0pnsfqmudlabnll.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
